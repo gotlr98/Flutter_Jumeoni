678b03d41091b3185be9896ea51ed74106235bf0
gotlr98 <gotlr98@naver.com>
2024-02-24 13:28:25 +0900

    install base packages

---
+++ flutter_jumeoni/lib/pages/login.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+      ),
+    );
+  }
+}
