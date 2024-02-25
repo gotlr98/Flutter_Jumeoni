10f2021bc3ae7b880898c803a90c6c47c095d114
gotlr98 <gotlr98@naver.com>
2024-02-25 22:22:54 +0900

    add apple login button & title

--- flutter_jumeoni/lib/pages/login.dart
+++ flutter_jumeoni/lib/pages/login.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class Login extends StatelessWidget {
+  const Login({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("주(酒)머니"),
+            SignInWithAppleButton(
+              onPressed: () async {
+                final credential = await SignInWithApple.getAppleIDCredential(
+                  scopes: [
+                    AppleIDAuthorizationScopes.email,
+                    AppleIDAuthorizationScopes.fullName,
+                  ],
+                );
+
+                print(credential);
+
+                // Now send the credential (especially `credential.authorizationCode`) to your server to create a session
+                // after they have been validated with Apple (see `Integration` section for more information on how to do this)
+              },
+            )
+          ],
         ),
       ),
     );
