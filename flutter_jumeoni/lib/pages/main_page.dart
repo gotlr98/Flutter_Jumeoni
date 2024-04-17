38da82c70e9a2a0513832cf69a991f0de5c8ff34
gotlr98 <gotlr98@naver.com>
2024-04-17 13:46:30 +0900

    change main page scrollable

--- flutter_jumeoni/lib/pages/main_page.dart
+++ flutter_jumeoni/lib/pages/main_page.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -10,7 +10,7 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var curUser = FirebaseAuth.instance.currentUser;
+    // var curUser = FirebaseAuth.instance.currentUser;
     // var email = curUser?.email;
 
     return Scaffold(
@@ -21,48 +21,59 @@ class MainPage extends StatelessWidget {
                   ? const Center(
                       child: CircularProgressIndicator.adaptive(),
                     )
-                  : GridView.builder(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 2,
-                              mainAxisSpacing: 10,
-                              crossAxisSpacing: 10,
-                              childAspectRatio: 9 / 16),
-                      itemCount: snapshot.data!.docs.length,
-                      itemBuilder: (context, index) {
-                        final url = snapshot.data!.docs[index]['url'];
+                  : SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          GridView.builder(
+                            scrollDirection: Axis.vertical,
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                                    crossAxisCount: 2,
+                                    crossAxisSpacing: 5,
+                                    mainAxisExtent: 256),
+                            itemCount: snapshot.data!.docs.length,
+                            itemBuilder: (context, index) {
+                              final url = snapshot.data!.docs[index]['url'];
 
-                        return InkWell(
-                          onTap: () {
-                            // var rating = snapshot.data!.docs[index]['rating'];
-                            var drinkName =
-                                snapshot.data!.docs[index]['drink_name'];
-                            var drinkPrice =
-                                snapshot.data!.docs[index]['drink_price'];
-                            Get.toNamed("/detail_page", arguments: {
-                              // "rating": rating,
-                              "drink_name": drinkName,
-                              "drink_price": drinkPrice,
-                              // "user_name": email,
-                            });
-                          },
-                          child: AspectRatio(
-                            aspectRatio: 9 / 16,
-                            child: Column(
-                              children: [
-                                Image.network(
-                                  url,
-                                  width: double.infinity,
-                                  fit: BoxFit.cover,
+                              return InkWell(
+                                onTap: () {
+                                  // var rating = snapshot.data!.docs[index]['rating'];
+                                  var drinkName =
+                                      snapshot.data!.docs[index]['drink_name'];
+                                  var drinkPrice =
+                                      snapshot.data!.docs[index]['drink_price'];
+                                  Get.toNamed("/detail_page", arguments: {
+                                    // "rating": rating,
+                                    "drink_name": drinkName,
+                                    "drink_price": drinkPrice,
+                                    // "user_name": email,
+                                  });
+                                },
+                                child: AspectRatio(
+                                  aspectRatio: 9 / 16,
+                                  child: Column(
+                                    children: [
+                                      const SizedBox(height: 5),
+                                      Expanded(
+                                        child: Image.network(
+                                          url,
+                                          width: double.infinity,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                      Text(snapshot.data!.docs[index]
+                                          ["drink_name"]),
+                                      const SizedBox(height: 5)
+                                    ],
+                                  ),
                                 ),
-                                Text(snapshot.data!.docs[index]["drink_name"]),
-                              ],
-                            ),
+                              );
+                            },
                           ),
-                        );
-                      },
+                        ],
+                      ),
                     );
             }));
   }
